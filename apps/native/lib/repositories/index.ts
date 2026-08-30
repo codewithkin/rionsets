92fcf0b2dd355a125a5b06e885f2b98e1db86ef1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-08-30 16:42:45 +0200

    chore(repos): add repositories barrel export

---
+++ apps/native/lib/repositories/index.ts
@@ -0,0 +1,4 @@
+export { ExerciseRepository, EXERCISE_LIBRARY } from "./exercise-repo";
+export { WorkoutRepository, SetRepository } from "./workout-repo";
+export { RoutineRepository } from "./routine-repo";
+export type { SeedExercise } from "./exercise-repo";
