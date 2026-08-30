dcd093d355643bf82dcc5cac90bea8fead47e155
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-08-30 16:01:59 +0200

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@iron-sets/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
